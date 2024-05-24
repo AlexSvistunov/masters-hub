cfcf53d4f67d1efc2432c51655ce54274ad3a90d
Alexey Svistunov <xdayx53@gmail.com>
2024-05-24 06:17:51 +0300

    connect state(redux), localstorage, html theme

--- src/store/slices/ThemeSlice.js
+++ src/store/slices/ThemeSlice.js
@@ -1,24 +1,27 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  currentTheme: localStorage.getItem("currentTheme")
+  currentTheme: localStorage.getItem('currentTheme') ?  localStorage.getItem('currentTheme') : 'dark'
 };
 
 const themeSlice = createSlice({
   name: "currentTheme",
   initialState,
   reducers: {
-    changeTheme: (state) => {
-      const localStorageTheme = localStorage.getItem('currentTheme')
-      const newTheme = state.currentTheme === "dark" ? "light" : "dark";
-      state.currentTheme = newTheme;
+    startTheme: (state) => {
+      document.querySelector('html').setAttribute('data-theme', state.currentTheme)
+    },
 
-      document.querySelector("html").setAttribute("data-theme", newTheme);
-      localStorage.setItem("currentTheme", newTheme);
+    changeTheme: (state) => {
+     const newTheme = state.currentTheme === 'dark' ? 'light' : 'dark'
+     state.currentTheme = newTheme
+
+     localStorage.setItem('currentTheme', newTheme)
+     document.querySelector('html').setAttribute('data-theme', state.currentTheme)
     },
   },
 });
 
 export default themeSlice.reducer;
 
-export const { changeTheme } = themeSlice.actions;
+export const { startTheme, changeTheme } = themeSlice.actions;
