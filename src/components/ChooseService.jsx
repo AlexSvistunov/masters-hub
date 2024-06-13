e317dd8a61965071009b337a211a9893037f39d8
AlexeySvistunov <xdayx53@gmail.com>
2024-06-13 18:19:05 +0300

    update enrollService modal steps

--- src/components/ChooseService.jsx
+++ src/components/ChooseService.jsx
@@ -30,6 +30,12 @@ const ChooseService = (props) => {
     getEnrollServices();
   }, []);
 
+  // if(!enrollServices.length) {
+  //   return (
+  //     <div className="flex justify-center items-center text-3xl w-full h-full">Ничего не найдено!</div>
+  //   )
+  // }
+
   return (
     <>
       <div className="flex justify-between items-center">
@@ -45,9 +51,9 @@ const ChooseService = (props) => {
           </svg>
         </button>
 
-        <span>{props.array[props.step]}</span>
+        <span className="inline-block mx-auto">{props.array[props.step]}</span>
 
-        <button onClick={props.nextStep}>
+        {/* <button onClick={props.nextStep}>
           <svg
             className="h-6 w-6 fill-current md:h-8 md:w-8"
             xmlns="http://www.w3.org/2000/svg"
@@ -57,58 +63,64 @@ const ChooseService = (props) => {
           >
             <path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z"></path>
           </svg>
-        </button>
+        </button> */}
       </div>
 
-      <div className="py-5">
-        <label className="input input-bordered flex items-center gap-2">
-          <input type="text" className="grow" placeholder="Search" />
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 16 16"
-            fill="currentColor"
-            className="w-4 h-4 opacity-70"
-          >
-            <path
-              fillRule="evenodd"
-              d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
-              clipRule="evenodd"
-            />
-          </svg>
-        </label>
-      </div>
+      {enrollServices.length ? (
+        <div>
+        <div className="py-5">
+          <label className="input input-bordered flex items-center gap-2">
+            <input type="text" className="grow" placeholder="Search" />
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 16 16"
+              fill="currentColor"
+              className="w-4 h-4 opacity-70"
+            >
+              <path
+                fillRule="evenodd"
+                d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </label>
+        </div>
 
-      <div className="flex items-center gap-4 my-4">
-        {enrollServices.map((service, index) => (
-          <button className="btn btn-primary" key={index}>
-            {Object.keys(service)}
-          </button>
-        ))}
-      </div>
+        <div className="flex items-center gap-4 my-4">
+          {enrollServices.map((service, index) => (
+            <button className="btn btn-primary" key={index}>
+              {Object.keys(service)}
+            </button>
+          ))}
+        </div>
 
-      <div className="list flex flex-col gap-5">
-     
-        {enrollServices.map((enrollService, index) => (
-          <div
-            className="flex flex-col gap-3"
-            key={index}
-          >
-            {Object.values(enrollService).map(
-              (enrollServiceArray, innerIndex) => (
-               <div key={innerIndex}>
-                <h3 className="text-3xl mb-2">{Object.keys(enrollService)}</h3>
-                 <div className="flex flex-col gap-3">
-                 {enrollServiceArray.map((enService, innerInnerIndex) => (
-                  <ServiceItem enService={enService} key={innerInnerIndex} step={props.step} setStep={props.setStep}/>
-                ))}
-                 </div>
-               </div>
-              )
-                
-            )}
-          </div>
-        ))}
+        <div className="list flex flex-col gap-5">
+          {enrollServices.map((enrollService, index) => (
+            <div className="flex flex-col gap-3" key={index}>
+              {Object.values(enrollService).map(
+                (enrollServiceArray, innerIndex) => (
+                  <div key={innerIndex}>
+                    <h3 className="text-3xl mb-2">
+                      {Object.keys(enrollService)}
+                    </h3>
+                    <div className="flex flex-col gap-3">
+                      {enrollServiceArray.map((enService, innerInnerIndex) => (
+                        <ServiceItem
+                          enService={enService}
+                          key={innerInnerIndex}
+                          step={props.step}
+                          setStep={props.setStep}
+                        />
+                      ))}
+                    </div>
+                  </div>
+                )
+              )}
+            </div>
+          ))}
+        </div>
       </div>
+      ) : <div className="flex justify-center items-center text-3xl w-full h-full text-center">Ничего не найдено! Иван посмотри других мастеров или салоны, возможно там что-то есть. А ВООБЩЕ СДЕЛАЙ НОРМАЛЬНУЮ БД СУКА</div>}
     </>
   );
 };
--- src/components/ServiceItem.jsx
+++ src/components/ServiceItem.jsx
@@ -1,29 +1,32 @@
-
-
-const ServiceItem = ({enService, step, setStep}) => {
+const ServiceItem = ({ enService, step, setStep }) => {
   return (
-    <div className="item rounded-lg bg-base-300 p-3" >
-                    <div className="flex gap-6 items-center mb-2">
-                      <img
-                        src={`/backend/masterhub${enService.photo}`}
-                        alt=""
-                        className="w-16 h-16 object-cover rounded-lg"
-                      />
-                      <h3 className="text-2xl">{enService.title}</h3>
-                    </div>
+    <div className="item rounded-lg bg-base-300 p-3">
+      <div className="flex gap-6 items-center mb-2">
+        <img
+          src={`/backend/masterhub${enService.photo}`}
+          alt=""
+          className="w-16 h-16 object-cover rounded-lg"
+        />
+        <h3 className="text-2xl">{enService.title}</h3>
+      </div>
 
-                    <div className="mt-auto flex justify-between items-center">
-                    <div className="flex gap-1 flex-col mb-2">
-                      <span className=" text-slate-500 text-xl">{enService.price} RUB</span>
-                      {/* <span>2 часа</span> */}
-                    </div>
+      <div className="mt-auto flex justify-between items-center">
+        <div className="flex gap-1 flex-col mb-2">
+          <span className=" text-slate-500 text-xl">{enService.price} RUB</span>
+          {/* <span>2 часа</span> */}
+        </div>
 
-                    <div className="flex justify-end">
-                      <button className="btn btn-primary text-base" onClick={() => setStep(step + 1)}>Выбрать</button>
-                    </div>
-                    </div>
-                  </div>
-  )
-}
+        <div className="flex justify-end">
+          <button
+            className="btn btn-primary text-base"
+            onClick={() => setStep(step + 1)}
+          >
+            Выбрать
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default ServiceItem
+export default ServiceItem;
