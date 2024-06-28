c2ae3b889a19fcc1ca31cdc1fa498ac238fe0dbe
AlexeySvistunov <xdayx53@gmail.com>
2024-06-28 11:16:49 +0300

    fix rating (if It's int - rating + .0 : rating)

--- src/main.jsx
+++ src/main.jsx
@@ -29,5 +29,5 @@ ReactDOM.createRoot(document.getElementById("root")).render(
 
 // склонения
 // посмотреть все
-// check last commit title
+// adaptive cards
 
--- src/pages/MasterPage.jsx
+++ src/pages/MasterPage.jsx
@@ -244,16 +244,16 @@ const MasterPage = () => {
                     >
                       <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
                     </svg>
-                    <p className="ms-2 text-sm font-bold">
-                      {masterData?.reviews?.average_rating}
+                    <p className="ms-1 text-sm font-bold">
+                      {formattedRating}
                     </p>
-                    <span className="w-1 h-1 mx-1.5 bg-gray-500 rounded-full dark:bg-gray-400"></span>
-                    <span
+                    {/* <span className="w-1 h-1 mx-1.5 bg-gray-500 rounded-full dark:bg-gray-400"></span> */}
+                    {masterData?.reviews?.count > 0 ? <span
                       href="#"
-                      className="text-sm font-medium  underline hover:no-underline"
+                      className="text-sm font-medium  underline hover:no-underline mx-3"
                     >
-                      {masterData?.reviews?.count} отзыва
-                    </span>
+                       {masterData?.reviews?.count}  отзывов 
+                    </span> : null}
                   </div>
                 </div>
                 <div className="user-reviews max-w-150 w-full">
