acce363f046710a574bc3360cacff36f0825dce9
Alexey Svistunov <xdayx53@gmail.com>
2024-07-02 16:44:45 +0300

    add worksExample component

--- src/pages/MasterPage.jsx
+++ src/pages/MasterPage.jsx
@@ -14,6 +14,7 @@ import "swiper/css/pagination";
 import "swiper/css/navigation";
 import Raiting from "../components/Raiting";
 import Services from "../components/Services";
+import WorksExample from "../components/WorksExample";
 
 const MasterPage = () => {
   const [masterData, setMasterData] = useState({});
@@ -87,40 +88,9 @@ const MasterPage = () => {
               </pre>
             </div>
 
-            {/* услуги */}
-    
-            <Services masterData={masterData}/>
+            <Services masterData={masterData} />
 
-            {/*  примеры работ*/}
-            <div className="bg-base-200 p-5 rounded-2xl mb-5">
-              <div className="flex gap-2 items-center mb-5">
-                <h3 className="text-3xl">Примеры работ</h3>
-                <span className="text-2xl text-primary">
-                  {masterData?.images_work?.length}
-                </span>
-              </div>
-
-              <div className="flex justify-center w-full">
-                <Swiper
-                  modules={[Navigation, Pagination]}
-                  slidesPerView={"auto"}
-                  spaceBetween={30}
-                  pagination={{ clickable: true }}
-                  navigation
-                >
-                  {masterData?.images_work?.map((workImage, index) => (
-                    <div key={workImage.image}>
-                      <SwiperSlide className="myCard">
-                        <img
-                          className="rounded-lg w-40 h-40 object-cover"
-                          src={`/backend/masterhub${workImage.image}`}
-                        ></img>
-                      </SwiperSlide>
-                    </div>
-                  ))}
-                </Swiper>
-              </div>
-            </div>
+            <WorksExample masterData={masterData} />
 
             {masterData?.specialists?.length ? (
               <div className="bg-base-200 p-5 rounded-2xl mb-5">
