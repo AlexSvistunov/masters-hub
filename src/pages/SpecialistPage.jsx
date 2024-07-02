008e9fa689b82f8344e6cde5df1b4cded50ae0d1
Alexey Svistunov <xdayx53@gmail.com>
2024-07-02 17:12:52 +0300

    add specialistPage

--- src/pages/SpecialistPage.jsx
+++ src/pages/SpecialistPage.jsx
@@ -1,8 +1,50 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { URL } from "/src/utils/backend-url";
+import Services from "../components/Services";
+import Header from "../components/Header";
 
 const SpecialistPage = () => {
-  return (
-    <div>SpecialistPage</div>
-  )
-}
+  const [specialistData, setSpecialistData] = useState({});
+  console.log(specialistData);
 
-export default SpecialistPage
+  const { id } = useParams();
+  const getSpecialistData = async () => {
+    const response = await fetch(`${URL}/api/specialist/${id}/`);
+    const data = await response.json();
+    setSpecialistData(data);
+  };
+
+  useEffect(() => {
+    getSpecialistData();
+  }, []);
+
+  return (
+    <>
+    <Header/>
+    <section className="py-40">
+      <div className="container mx-auto">
+        <div className="flex gap-8 mb-5">
+          <img
+            src={`/backend/masterhub/static${specialistData?.photo}`}
+            width={340}
+            height={340}
+            className="rounded-lg"
+          ></img>
+
+          <div className="bg-base-200 p-8 rounded-2xl flex-auto">
+           <div className="mb-4">
+           <h3 className="text-3xl">{specialistData?.name}</h3>
+           <span className="text-gray-500 text-xl">{specialistData?.job}</span>
+           </div>
+            <p className="text-lg">{specialistData?.description}</p>
+          </div>
+        </div>
+
+        <Services masterData={specialistData} />
+      </div>
+    </section></>
+  );
+};
+
+export default SpecialistPage;
