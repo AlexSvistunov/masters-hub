4820b92d561c8c713ba2c7c450a9fc7f0381d3a0
Alexey Svistunov <xdayx53@gmail.com>
2024-08-25 13:53:53 +0300

    add react-hook-form validation

--- src/pages/CreateProfile.jsx
+++ src/pages/CreateProfile.jsx
@@ -11,58 +11,19 @@ const CreateProfile = () => {
     handleSubmit,
     watch,
     formState: { errors },
+    setError,
   } = useForm();
 
-  const [profileFields, setProfileFields] = useState({
-    name: "",
-    address: "",
-    phone: "",
-    specialization: "",
-    link_vk: "",
-    link_tg: "",
-    description: "",
-  });
-
-  // const testObject = {
-  //   name: "Alex",
-  //   address: "Pushkina",
-  //   phone: "79999999999",
-  //   specialization: "master",
-  //   link_vk: "vk.com",
-  //   link_tg: "tgweb.com",
-  //   description: "крутой тип",
-  //   time_relax: "00:30:00"
-  // };
-
-  console.log(profileFields);
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setProfileFields({
-      ...profileFields,
-      [name]: value,
-    });
-  };
-
   const createProfile = async () => {
-    const {
-      name,
-      address,
-      phone,
-      specialization,
-      link_vk,
-      link_tg,
-      description,
-    } = profileFields;
 
     const testObject = {
-      name: watch('name'),
-      address: watch('adress'),
-      phone: watch('phone'),
-      specialization: watch('specialization'),
-      link_vk: watch('link_vk'),
-      link_tg: watch('link_tg'),
-      description: watch('description'),
+      name: watch("name"),
+      address: watch("adress"),
+      phone: watch("phone"),
+      specialization: watch("specialization"),
+      link_vk: watch("link_vk"),
+      link_tg: watch("link_tg"),
+      description: watch("description"),
       time_relax: "00:30:00",
     };
 
@@ -78,15 +39,20 @@ const CreateProfile = () => {
 
         body: JSON.stringify(testObject),
       });
+
       const data = await response.json();
+      if(!response.ok) {
+        console.log('data')
+        setError('link_vk', { type: 'custom', message: data.link_vk[0] })
+        console.log(errors)
+      }
       console.log(data);
     } catch (error) {
       console.error("An error occurred:", error);
     }
   };
 
-  console.log(watch('name'))
-
+  console.log(watch("name"));
 
   return (
     <div className="m-5">
@@ -97,63 +63,87 @@ const CreateProfile = () => {
             className="input input-bordered"
             placeholder="Имя"
             name="name"
-            // value={profileFields.name}
-            // onChange={handleChange}
-            {...register('name', {required: true})}
+            {...register("name", {
+              required: "Поле обязательно к заполнению!",
+            })}
           ></input>
+
+          <span className="text-red-500">
+            {errors.name && errors.name.message}
+          </span>
           <input
             className="input input-bordered"
             placeholder="Адрес"
             name="address"
-            value={profileFields.address}
-            onChange={handleChange}
-            {...register('address', {required: true})}
+            {...register("address", {
+              required: "Поле обязательно к заполнению!",
+            })}
           ></input>
+          <span className="text-red-500">
+            {errors.address && errors.address.message}
+          </span>
           <input
             className="input input-bordered"
             placeholder="Телефон"
             name="phone"
-            value={profileFields.phone}
-            onChange={handleChange}
-            {...register('phone', {required: true})}
+            {...register("phone", {
+              required: "Поле обязательно к заполнению!",
+            })}
           ></input>
+          <span className="text-red-500">
+            {errors.phone && errors.phone.message}
+          </span>
           <input
             className="input input-bordered"
             placeholder="Специализация"
             name="specialization"
-            value={profileFields.specialization}
-            onChange={handleChange}
-            {...register('specialization', {required: true})}
+            {...register("specialization", {
+              required: "Поле обязательно к заполнению!",
+              pattern: {  
+                value: /^(master|studio)$/,  
+                message: 'Специализация должна быть либо "master", либо "studio"',  
+            },  
+            })}
           ></input>
+          <span className="text-red-500">
+            {errors.specialization && errors.specialization.message}
+          </span>
           <input
             className="input input-bordered"
             placeholder="Ссылка на VK"
             name="link_vk"
-            value={profileFields.link_vk}
-            onChange={handleChange}
-            {...register('link_vk', {required: true})}
+            {...register("link_vk", {
+              required: "Поле обязательно к заполнению!",
+            })}
           ></input>
+          <span className="text-red-500">
+            {errors.link_vk && errors.link_vk.message}
+          </span>
           <input
             className="input input-bordered"
             placeholder="Ссылка на TG"
             name="link_tg"
-            value={profileFields.link_tg}
-            onChange={handleChange}
-            {...register('link_tg', {required: true})}
+            {...register("link_tg", {
+              required: "Поле обязательно к заполнению!",
+            })}
           ></input>
+          <span className="text-red-500">
+            {errors.link_tg && errors.link_tg.message}
+          </span>
           <input
             className="input input-bordered"
             placeholder="Описание"
             name="description"
-            value={profileFields.description}
-            onChange={handleChange}
-            {...register('description', {required: true})}
+            {...register("description", {
+              required: "Поле обязательно к заполнению!",
+            })}
           ></input>
+          <span className="text-red-500">
+            {errors.description && errors.description.message}
+          </span>
         </div>
 
-        <button className="btn btn-accent my-2">
-          Создать профиль
-        </button>
+        <button className="btn btn-accent my-2">Создать профиль</button>
       </form>
     </div>
   );
