e86013fcb477c1204c705a729d40648d8fb95601
Alexey Svistunov <112976775+AlexSvistunov@users.noreply.github.com>
2024-09-26 22:02:15 +0300

    worktime

--- src/pages/business/BusinessWorkTimeSpec.jsx
+++ src/pages/business/BusinessWorkTimeSpec.jsx
@@ -1,55 +1,80 @@
-import { Link, useParams } from "react-router-dom";
-import BusinessLayout from "../../components/business/BusinessLayout";
-import { DayPicker } from "react-day-picker";
-import { useState } from "react";
+import { Link, useParams } from 'react-router-dom'
+import BusinessLayout from '../../components/business/BusinessLayout'
+import { DayPicker } from 'react-day-picker'
+import { useEffect, useState } from 'react'
 
-import { Pencil } from "lucide-react";
+import { Pencil } from 'lucide-react'
+import URL from '../../utils/backend-url'
+import useAuth from '../../hooks/useAuth'
+import { formatDate } from '../../utils/formatDate'
 
 const BusinessWorkTimeSpec = () => {
-  const { id } = useParams();
+	const { id } = useParams()
 
-  const myTime = [
-    "11:00",
-    "12:00",
-    "11:00",
-    "12:00",
-    "11:00",
-    "12:00",
-    "11:00",
-    "12:00",
-  ];
+	const myTime = [
+		'11:00',
+		'12:00',
+		'11:00',
+		'12:00',
+		'11:00',
+		'12:00',
+		'11:00',
+		'12:00',
+	]
 
-  const getWorkTime = () => {
-    
-  }
+	const { currentToken } = useAuth()
 
-  const [selectedDays, setSelectedDays] = useState([]);
-  const [selected, setSelected] = useState(new Date())
+	const [selectedDays, setSelectedDays] = useState([])
+	const [selected, setSelected] = useState(new Date())
 
-  return (
-    <BusinessLayout>
-      <div className="flex items-start gap-10 flex-col laptop:flex-row">
-        <DayPicker mode="single" selected={selected} onSelect={(day) => setSelectedDays(day)} />
+	const getWorkTime = async () => {
+		const date = formatDate(selectedDays)
+		const headers = {}
+		if (currentToken) {
+			headers.Authorization = `Token ${currentToken}`
+		}
+		const response = await fetch(`${URL}/api/admin-panel/work-time/${id}/?date=${date}`, {
+			method: 'GET',
+			headers: {
+				Authorization: `Token ${currentToken}`,
+			},
+		})
+		const data = await response.json()
+		console.log(data)
+	}
 
-        <div className="py-4 px-9 border border-gray-700 max-w-lg w-full flex flex-wrap gap-2 rounded-xl min-h-32 items-start relative">
-          {myTime?.map((time) => (
-            <>
-              <div
-                key={time}
-                className="p-2 border border-2 border-accent rounded-lg"
-              >
-                {time}
-              </div>
-            </>
-          ))}
+	useEffect(() => {
+		getWorkTime()
+	}, [selectedDays])
 
-          <Link state={selectedDays} to={`/business/work-time/${id}/edit`}>
-            <Pencil color={"#00CDB7"} className="absolute top-2 right-2" />
-          </Link>
-        </div>
-      </div>
-    </BusinessLayout>
-  );
-};
+	return (
+		<BusinessLayout>
+			<div className='flex items-start gap-10 flex-col laptop:flex-row'>
+				<DayPicker
+					mode='single'
+					selected={selected}
+					onSelect={day => setSelectedDays(day)}
+				/>
 
-export default BusinessWorkTimeSpec;
+				<div className='py-4 px-9 border border-gray-700 max-w-lg w-full flex flex-wrap gap-2 rounded-xl min-h-32 items-start relative'>
+					{myTime?.map(time => (
+						<>
+							<div
+								key={time}
+								className='p-2 border border-2 border-accent rounded-lg'
+							>
+								{time}
+							</div>
+						</>
+					))}
+
+					<Link state={selectedDays} to={`/business/work-time/${id}/edit`}>
+						<Pencil color={'#00CDB7'} className='absolute top-2 right-2' />
+					</Link>
+				</div>
+			</div>
+		</BusinessLayout>
+	)
+}
+
+export default BusinessWorkTimeSpec
